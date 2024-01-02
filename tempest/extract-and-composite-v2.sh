11f6b4b623efa94411a32785bcb809bfd66795e6
Colin Zarzycki <zarzycki@ucar.edu>
2024-01-01 19:35:25 -0700

    e and c updates

--- tempest/extract-and-composite-v2.sh
+++ tempest/extract-and-composite-v2.sh
@@ -3,10 +3,10 @@
 ##=======================================================================
 #PBS -N tempest.par
 #PBS -A P93300642
-#PBS -l walltime=04:00:00
+#PBS -l walltime=06:00:00
 #PBS -q main
 #PBS -j oe
-#PBS -l select=1:ncpus=16:mpiprocs=16
+#PBS -l select=1:ncpus=1:mpiprocs=1
 ################################################################
 
 set -e
@@ -18,19 +18,22 @@ cd /glade/u/home/$LOGNAME/sw/tgw-tc/tempest
 NCLBIN=/glade/u/apps/derecho/23.06/spack/opt/spack/ncl/6.6.2/gcc/7.5.0/q5mj/bin/
 
 MPICOMMAND="mpiexec"
-#NCPU=16
-#MPICOMMAND="mpirun --np ${NCPU} --hostfile $PBS_NODEFILE"
+
+#module list
+#module avail
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/pmi/6.1.10/lib
+#echo $LD_LIBRARY_PATH
 
 ############ USER OPTIONS #####################
 
 ## Unique string (useful for processing multiple data sets in same folder
-UQSTR=CONUS_TGW_WRF_Historical
+UQSTR=CONUS_TGW_WRF_SSP585_HOT_NEAR
 
 ## Path to TempestExtremes binaries (I will maintain this)
 TEMPESTEXTREMESDIR=/glade/u/home/zarzycki/work/derecho/tempestextremes/
 
 ### Where do we want to write files?
-PATHTOFILES=/glade/derecho/scratch/zarzycki/tempest/tgw/
+PATHTOFILES=/glade/derecho/scratch/zarzycki/tempest/composites/
 
 ### Where is the WRF data located
 TGWDIR=/glade/derecho/scratch/zarzycki/TGW
@@ -39,7 +42,7 @@ AUX2DIR=${TGWDIR}/${UQSTR}/aux2/
 AUX3DIR=${TGWDIR}/${UQSTR}/PRECT_6h/
 
 ### What is the traj file from the WRF runs?
-TRAJFILENAME=./cleaned/traj.Historical_time_cleaned.txt
+TRAJFILENAME=./cleaned/traj.hot_near_time_cleaned.txt
 
 #######################################################################################
 
@@ -60,16 +63,6 @@ ls ${AUX2DIR}/wrfout_d01_????*.nc > aux2_file_list.txt
 echo "Gathering Pre files"
 ls ${AUX3DIR}/wrfout_d01_????*.nc > aux3_file_list.txt
 
-rm -fv filt_output_files.txt
-echo "Create filtered files"
-PRECIPFILES=`ls ${AUXDIR}/wrfout_d01_????*.nc`
-for g in $PRECIPFILES
-do
-  filename=${g}
-  just_filename=`basename $g`
-  echo "${PATHTOFILES}/${just_filename}_filtered.nc" >> filt_output_files.txt
-done
-
 ## What is the name of the final script we want to create?
 FINALTRAJ=${TRAJFILENAME}_final
 
@@ -77,25 +70,11 @@ FINALTRAJ=${TRAJFILENAME}_final
 echo "Appending any other diags"
 $NCLBIN/ncl append-stats.ncl 'thefile="'${TRAJFILENAME}'"' 'filename="'${FINALTRAJ}'"'
 
-## First, we want to take the trajectory file, take the WRF files, and mask off grid cells *beyond* 5deg of a TC center point defined by the trajectory.
-echo "extracting TC precip using a set radius of 3 GCD"
-$MPICOMMAND ${TEMPESTEXTREMESDIR}/bin/NodeFileFilter --in_nodefile ${TRAJFILENAME} --in_fmt "lon,lat,slp,wind" --in_data_list "aux_file_list.txt" --out_data_list "filt_output_files.txt" --bydist 5.0 --var "IVTE,IVTN,T200,SLP,U10,V10,OLR" --preserve "XLAT,XLONG" --regional --fillvalue "-9999999.9" --maskvar "mask" --latname "XLAT" --lonname "XLONG"
-rm -fv log0*.txt
-
-## Here is an example of taking the trajectory file and getting a subfile out that *only* includes TCs with SLP < 965mb
-#echo "Filter strong cyclones"
-#singlefile=`head -1 filt_output_files.txt`
-#${TEMPESTEXTREMESDIR}/bin/NodeFileEditor --in_nodefile ${TRAJFILENAME} --in_data ${singlefile} --in_fmt "lon,lat,slp,wind" --out_nodefile ${TRAJFILENAME}_strong.txt --out_fmt "lon,lat,slp,wind" --colfilter "slp,<=,965." --latname "XLAT" --lonname "XLONG"
-
-## Here is an example of creating a simple histogram map with 5x5deg grid boxes of the strong TC track density
-#echo "Create a density map of all intense storms"
-#${TEMPESTEXTREMESDIR}/bin/HistogramNodes --in ${TRAJFILENAME}_strong.txt --nlat 36 --nlon 72 --iloncol 3 --ilatcol 4 --lon_begin -180.0 --lon_end 180.0 --out "${PATHTOFILES}/density_${UQSTR}.nc"
-
 ## Here is an example of compositing the storms we masked off above into one big average using the filtered trajectory file as our list of which storms to composite.
 echo "Compositing"
 ${TEMPESTEXTREMESDIR}/bin/NodeFileCompose --in_nodefile ${TRAJFILENAME} --in_fmt "lon,lat,slp,wind" --in_data_list aux_file_list.txt --missingdata --var "U10,V10,SLP,OLR,IVTE,IVTN" --regional --max_time_delta "2h" --out_data "${PATHTOFILES}/new_composite_aux_${UQSTR}.nc" --dx 0.1 --resx 100 --op "mean" --latname "XLAT" --lonname "XLONG" --snapshots
-${TEMPESTEXTREMESDIR}/bin/NodeFileCompose --in_nodefile ${TRAJFILENAME} --in_fmt "lon,lat,slp,wind" --in_data_list aux2_file_list.txt --missingdata --var "TMQ,U850,V850" --regional --max_time_delta "2h" --out_data "${PATHTOFILES}/new_composite1_aux2_${UQSTR}.nc" --dx 0.1 --resx 100 --op "mean" --latname "XLAT" --lonname "XLONG" --snapshots
-${TEMPESTEXTREMESDIR}/bin/NodeFileCompose --in_nodefile ${TRAJFILENAME} --in_fmt "lon,lat,slp,wind" --in_data_list aux3_file_list.txt --missingdata --var "PRECT" --regional --max_time_delta "2h" --out_data "{PATHTOFILES}/new_composite_aux3_${UQSTR}.nc" --dx 0.1 --resx 100 --op "mean" --latname "XLAT" --lonname "XLONG" --snapshots
+${TEMPESTEXTREMESDIR}/bin/NodeFileCompose --in_nodefile ${TRAJFILENAME} --in_fmt "lon,lat,slp,wind" --in_data_list aux2_file_list.txt --missingdata --var "TMQ,U850,V850" --regional --max_time_delta "2h" --out_data "${PATHTOFILES}/new_composite_aux2_${UQSTR}.nc" --dx 0.1 --resx 100 --op "mean" --latname "XLAT" --lonname "XLONG" --snapshots
+${TEMPESTEXTREMESDIR}/bin/NodeFileCompose --in_nodefile ${TRAJFILENAME} --in_fmt "lon,lat,slp,wind" --in_data_list aux3_file_list.txt --missingdata --var "PRECT" --regional --max_time_delta "2h" --out_data "${PATHTOFILES}/new_composite_aux3_${UQSTR}.nc" --dx 0.1 --resx 100 --op "mean" --latname "XLAT" --lonname "XLONG" --snapshots
 
 ## Add rmw, wind@rmw, r8, ike to tempest files
 echo "Calculating additional derived diagnostics with NFE"
@@ -109,7 +88,6 @@ echo "Cleaning up!"
 rm -fv aux_file_list.txt
 rm -fv aux2_file_list.txt
 rm -fv aux3_file_list.txt
-rm -fv filt_output_files.txt
 rm -fv *${randomstr}
 
 ## Print some timing stats
@@ -119,4 +97,3 @@ printf "${tottime},extract_and_composite,${UQSTR}\n" >> timing.txt
 
 ## Gracefully exit the script
 exit
-
